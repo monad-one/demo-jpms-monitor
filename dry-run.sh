2b99ecc5429243d6d6094068bc867f6d79dea06a
nipa <nipa@codefx.org>
2017-03-20 12:27:54 +0100

    Create stats.fancy:
    * create new module
    * let monitor.statistics use stats.fancy
    * update poms and scripts to compile and run stats.fancy

--- dry-run.sh
+++ dry-run.sh
@@ -6,9 +6,11 @@ echo "--- LAUNCH ---"
 echo " > dry-run monitor"
 echo ""
 
-# the classpath is needed for Spark's dependencies
+# the classpath is needed for Spark's dependencies;
+# add the optional dependency stats.fancy
 java9 \
 	--module-path mods \
 	--class-path "libs/*" \
+	--add-modules stats.fancy \
 	--dry-run \
 	--module monitor
